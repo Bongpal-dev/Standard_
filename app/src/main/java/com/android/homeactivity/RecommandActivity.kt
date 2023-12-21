7c11764d3b67c2866c5af80fddf16c3a7b3da31b
YS <etgt777@naver.com>
2023-12-21 21:29:00 +0900

    coodinator, collabs complete

--- app/src/main/java/com/android/homeactivity/RecommandActivity.kt
+++ app/src/main/java/com/android/homeactivity/RecommandActivity.kt
@@ -1,54 +1,21 @@
 package com.android.homeactivity
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.FragmentActivity
 import com.android.homeactivity.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.tabs.TabLayoutMediator
+import com.android.homeactivity.databinding.ActivityRecommandBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class RecommandActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecommandBinding
 
-    val tag = "main_activity_test"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view =binding.root
+        binding = ActivityRecommandBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
-        topBannerAdapterConnect()
-        mainContentAdapterConnect()
         bottomNaviInit()
-    }
-
-    private fun topBannerAdapterConnect() {
-        binding.vpTopBanner.apply {
-            adapter = TopBannerAdapter(context as FragmentActivity)
-        }
-    }
-
-    private fun mainContentAdapterConnect() {
-        binding.vpWebtoonList.apply {
-            adapter = WebtoonListAdapter(context as FragmentActivity)
-        }
-        TabLayoutMediator(binding.tlWebtoonTabMenu, binding.vpWebtoonList) { tab, position ->
-            tab.text = when (position) {
-                0 -> "신작"
-                1 -> "매일+"
-                2 -> "월"
-                3 -> "화"
-                4 -> "수"
-                5 -> "목"
-                6 -> "금"
-                7 -> "토"
-                8 -> "일"
-                9 -> "완결"
-                else -> "금"
-            }
-        }.attach()
     }
 
     fun bottomNaviInit() {
@@ -58,11 +25,10 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.webtoon -> {
                     Log.i(tag, "웹툰")
+                    finish()
                     return@setOnItemSelectedListener true
                 }
                 R.id.recommand_end -> {
-                    val intent = Intent(this, RecommandActivity::class.java)
-                    startActivity(intent)
                     Log.i(tag, "추천완결")
                     return@setOnItemSelectedListener true
                 }
@@ -86,5 +52,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
--- app/src/main/java/com/android/homeactivity/WebtoonListAdapter.kt
+++ app/src/main/java/com/android/homeactivity/WebtoonListAdapter.kt
@@ -5,43 +5,23 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class WebtoonListAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-    private val num_page = 7
+    private val num_page = 10
 
     override fun getItemCount(): Int = num_page
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> {
-                Monday.newInstance("page1", "")
-            }
-
-            1 -> {
-                Monday.newInstance("page2", "")
-            }
-
-            2 -> {
-                Monday.newInstance("page3", "")
-            }
-
-            3 -> {
-                Monday.newInstance("page4", "")
-            }
-
-            4 -> {
-                Monday.newInstance("page5", "")
-            }
-
-            5 -> {
-                Monday.newInstance("page6", "")
-            }
-
-            6 -> {
-                Monday.newInstance("page7", "")
-            }
-
-            else -> {
-                Monday.newInstance("page8", "")
-            }
+            0 -> Monday.newInstance("page1", "")
+            1 -> Monday.newInstance("page2", "")
+            2 -> Monday.newInstance("page3", "")
+            3 -> Monday.newInstance("page4", "")
+            4 -> Monday.newInstance("page5", "")
+            5 -> Monday.newInstance("page6", "")
+            6 -> Monday.newInstance("page7", "")
+            7 -> Monday.newInstance("page8", "")
+            8 -> Monday.newInstance("page9", "")
+            9 -> Monday.newInstance("page10", "")
+            else -> Monday.newInstance("page8", "")
         }
     }
 }
